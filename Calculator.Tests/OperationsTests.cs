36e37020ad45074cf23ae2a8c4d82c33c41152e8
aharita <aharita@nearsoft.com>
2012-06-16 16:20:29 -0700

    added base project and tests

---
+++ Calculator.Tests/OperationsTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace Calculator.Tests
+{
+    [TestFixture]
+    public class OperationsTests
+    {
+        [Test]
+        public void CanSum()
+        {
+            Assert.That(Operations.Sum(1, 1) == 2);
+        }
+
+        [Test]
+        public void CanDivide()
+        {
+            Assert.That(Operations.Divide(1, 1) == 1);
+        }
+    }
+}
---
+++ Operations.cs
@@ -0,0 +1,25 @@
+﻿// A very naive calculator, which whole purpose is to create an MSBuild file and do some unit testing and
+// code coverage
+
+using System;
+
+namespace Calculator
+{
+    public static class Operations
+    {
+        public static int Sum(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static int Divide(int x, int y)
+        {
+            if (y == 0)
+            {
+                throw new Exception("Can't divide by 0");
+            }
+
+            return x / y;
+        }
+    }
+}
